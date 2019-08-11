2add61292a0a24d0a2c95d7f3ceb031a23a486cd
Mika Seppälä <mjseppala@gmail.com>
2019-08-11 14:32:41 +0300

    uibuild

---
+++ build/precache-manifest.7bc51738cfca8689d1d0fdd281c3506c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d52644fe9525c9ab7693f30cc421517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c6f22031257d1dcae8",
+    "url": "/static/js/2.72340b54.chunk.js"
+  },
+  {
+    "revision": "2041cae922d829858e95",
+    "url": "/static/js/main.5caf9d2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
